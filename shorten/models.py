dfdb305f8e0c65a8cc3f30e7c56cc5335c7dc323
Maciej Kwiek <mkwiek@mirantis.com>
2016-11-11 15:14:22 +0100

    models and migration

--- shorten/models.py
+++ shorten/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from shortly import settings
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    date_joined = models.DateTimeField()
+
+    def __str__(self):
+        return "{} {} {}".format(self.first, self.last_name, self.username)
+
+class Url(models.Model):
+    original_url = models.CharField(max_length=2000)
+    shortened_url = models.CharField(max_length=settings.SHORT_URL_MAX_LEN)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{} => {}".format(self.original_url, self.shortened_url)
--- shortly/settings.py
+++ shortly/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shorten.apps.ShortenConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SHORT_URL_MAX_LEN = 200
