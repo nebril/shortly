1b339293ddd18faf15fb884976c2f5293e69e01d
Maciej Kwiek <mkwiek@mirantis.com>
2016-11-12 12:32:48 +0100

    Add html template

--- shorten/views.py
+++ shorten/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the django app index.")
+    return render(request, "shorten/index.html", {})
 
 def item(request):
     return HttpResponse("item view")
